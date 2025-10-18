54285399817778dcb00fd0ebcf0c87e2601e7460
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-19 03:55:18 +0500

    Task2

---
+++ Tyuiu.ArkhipovaMD.Sprint3.Task2.V4.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ArkhipovaMD.Sprint3.Task2.V4.Lib
+{
+    public class DataService : ISprint3Task2V4
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double r = 1;
+            do
+            {
+                double re = Math.Pow((double)startValue / (double)Math.Pow((double)Math.Sin(1), -7), -2);
+                r = (double)r * re;
+                startValue++;
+            }while (startValue<=stopValue);
+            return Math.Round(r,3);
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint3.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ArkhipovaMD.Sprint3.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
