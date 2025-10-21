713f6a083eeb5be8740b0be91671dcf4b29dee8a
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-21 15:56:52 +0500

    Task5

---
+++ Tyuiu.ArkhipovaMD.Sprint3.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ArkhipovaMD.Sprint3.Task5.V16.Lib
+{
+    public class DataService : ISprint3Task5V16
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sum = 0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                sum += Math.Cos(i) + 4;
+            }
+            for (int i = startValue2; i <= stopValue2; i++)
+            {
+                sum += sum;
+            }
+            return Math.Round(sum);
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint3.Task5.V16/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint3.Task5.V16.Lib;
+int x = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int d = Convert.ToInt32(Console.ReadLine());
+DataService ds = new DataService();
+Console.WriteLine(ds.GetSumSumSeries(x,a,b,c,d));
