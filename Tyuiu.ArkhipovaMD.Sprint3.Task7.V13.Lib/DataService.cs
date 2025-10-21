fdf517bc6a7c25cfadc5e53d0f56b51683b759ef
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-21 21:39:11 +0500

    Task7

---
+++ Tyuiu.ArkhipovaMD.Sprint3.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.ArkhipovaMD.Sprint3.Task7.V13.Lib
+{
+    public class DataService : ISprint3Task7V13
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] m = new double[11];
+            int cnt = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (Math.Cos(x) == -1)
+                {
+                    m[cnt] = 0;
+                }
+                else
+                {
+                    m[cnt] = 3 * x + 2 - ((2 * x - x) / (Math.Cos(x) + 1));
+                }
+                cnt++;
+            }
+            return m;
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint3.Task7.V13/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint3.Task7.V13.Lib;
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+DataService ds = new DataService();
+foreach (double i in ds.GetMassFunction(a,b))
+{
+    Console.Write(Math.Round(i,2)+"\t");
+}
